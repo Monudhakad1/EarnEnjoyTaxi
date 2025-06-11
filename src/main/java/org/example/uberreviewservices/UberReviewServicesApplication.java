327f9a9cfdf79eaa315a08e82b6b2da2dbb14727
Monudhakad1 <monudhakad055@gmail.com>
2025-06-11 22:50:54 +0530

    Basic DB configuration

---
+++ src/main/java/org/example/uberreviewservices/UberReviewServicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.uberreviewservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberReviewServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberReviewServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/uberreviewservices/UberReviewServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.uberreviewservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
