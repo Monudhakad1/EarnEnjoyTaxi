d16fc6654754073e5c153ec145af6807700458f0
Monudhakad1 <monudhakad055@gmail.com>
2025-06-27 23:21:18 +0530

    Raw SQL QUERY done

--- src/main/java/org/example/uberreviewservices/Repositories/DriverRepository.java
+++ src/main/java/org/example/uberreviewservices/Repositories/DriverRepository.java
@@ -2,6 +2,7 @@ package org.example.uberreviewservices.Repositories;
 
 import org.example.uberreviewservices.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,8 @@ public interface DriverRepository extends JpaRepository<Driver, Long> {
     Optional<Driver> findByIdAndLicenseNumber(Long id, String licenseNumber);
 
     List<Driver> findAllByIdIn(List<Long> driverIds);
+
+    @Query(nativeQuery = true , value="SELECT * FROM driver WHERE id= :id AND license_number= :licenseNumber ")
+    Optional<Driver> rawFindByIdAndDrivingLicense(Long id , String licenseNumber);
+
 }
